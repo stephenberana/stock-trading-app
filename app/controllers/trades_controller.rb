a8e1d7db88974b77f684c36f8ef6a36ad6ef8dc2
Stephen Berana <stephenberana@gmail.com>
2021-11-22 09:22:22 +0800

    Sell transaction working!

--- app/controllers/trades_controller.rb
+++ app/controllers/trades_controller.rb
@@ -1,12 +1,9 @@
 class TradesController < ApplicationController
         # before_action :authenticate_user!
         before_action :setup
-        # around_action :deduct_from_balance, only: [:create]
-        
-        def index
-            # @trades = current_user.trades ||= nil
-            @trades = Trade.where(user_session)
 
+        def index
+            @trades = Trade.where(user_session)
         end
         
         def new
@@ -14,15 +11,61 @@ class TradesController < ApplicationController
             # @trade.build_stock
         end
 
-        def update
+        def edit
+            @trade = current_user.trades.find(params[:id])
         end
 
-        def create
+        def update
             client = IEX::Api::Client.new(
                 publishable_token: 'pk_92611ab063e242c8b3ccbed009db8f65', 
                 endpoint: 'https://cloud.iexapis.com/v1'
             )
+            stock_price = client.quote(params[:trade][:ticker]).latest_price
+            total_price = stock_price * params[:trade][:quantity].to_i
 
+            @trade = current_user.trades.find(params[:id])
+            
+            if @trade.update(trade_params)
+                current_user.balances.create(deposit: total_price)
+                redirect_to trades_path, notice: 'Stocks successfully sold.'
+            else
+                flash[:alert] = "Cannot continue with the transaction. Please check your inputs."
+                render :edit
+            end
+            # #declaring params
+            # quantity = params[:trade][:quantity]
+
+            # #start sell transaction
+            # if quantity && (quantity <= quantity)
+            #     @trade = Trade.update(trade_params)
+            #     # Rails.logger.info(@trade.errors.inspect)
+            #     if @trade.quantity.instance_of? Integer
+            #         @trade.price = stock_price
+            #         @trade.purchase_date = DateTime.now
+            #         @trade.company_name = company_name
+            #         @trade.ticker = company_ticker
+            #             begin 
+            #             @trade.save!
+            #             current_user.balances.create(deposit: total_price)
+            #             rescue ActiveRecord::RecordInvalid => invalid
+            #             puts invalid.record.errors
+            #             byebug
+            #             end
+            #         flash[:notice] = "Transaction successful!"
+            #         redirect_to trades_path
+            #     else
+            #         flash[:alert] = "Please enter the whole number of shares that you want to sell."
+            #         @trade.errors
+            #         redirect_to new_trade_path 
+            #     end
+            # else
+            #     flash[:alert] = "You cannot sell what you don't have."
+            #     @trade.errors
+            #     redirect_to new_trade_path
+            # end
+        end
+
+        def create
             #getting stock price information
             stock_price = client.quote(params[:trade][:ticker]).latest_price
             total_price = stock_price * params[:trade][:quantity].to_i
@@ -31,7 +74,7 @@ class TradesController < ApplicationController
             company_name = client.company(params[:trade][:ticker]).company_name
             company_ticker = params[:trade][:ticker]
 
-            #start transaction
+            #start buy transaction
             if @balance && (@balance > total_price)
                 @trade = Trade.create(trade_params)
                 Rails.logger.info(@trade.errors.inspect)
@@ -44,8 +87,7 @@ class TradesController < ApplicationController
                         @trade.save!
                         current_user.balances.create(withdrawals: total_price)
                         rescue ActiveRecord::RecordInvalid => invalid
-                        # puts invalid.record.errors
-                        byebug
+                        puts invalid.record.errors
                         end
                     flash[:notice] = "Transaction successful!"
                     redirect_to trades_path
@@ -53,16 +95,18 @@ class TradesController < ApplicationController
                 else
                     flash[:alert] = "Please enter the whole number of shares that you want to purchase."
                     @trade.errors
-                    redirect_to new_trade_path
-                    # Rails.logger.info(params[:trade])  
+                    redirect_to new_trade_path 
                 end
             else
-                byebug
                 flash[:alert] = "Cannot continue with transaction. Please add more funds to your account."
                 @trade.errors
-                redirect_to new_trade_path
-                # Rails.logger.info(params[:trade])            
+                redirect_to new_trade_path      
             end
+        end
+
+        def destroy
+            @trade.destroy
+            redirect_to trades_path, notice: "Transaction log successfully deleted."
         end
     
         private
@@ -72,8 +116,11 @@ class TradesController < ApplicationController
         end
 
         def setup
-            # @balance = Balance.find(current_user.id).deposit
-            # @balance = current_user.balance.total_balance
             @balance = Balance.where(user_session).total_balance
+
+            client = IEX::Api::Client.new(
+                publishable_token: 'pk_92611ab063e242c8b3ccbed009db8f65', 
+                endpoint: 'https://cloud.iexapis.com/v1'
+            )
         end
 end
